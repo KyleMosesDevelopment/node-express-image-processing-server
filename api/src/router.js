7a17c6e4d373a0b28c1bd0d67a844570fb597404
Kyle Moses <kmoses@stedwards.edu>
2022-06-09 12:48:08 -0500

    mod 2

---
+++ api/src/router.js
@@ -0,0 +1,38 @@
+const { Router } = require('express')
+const { diskStorage } = require('multer')
+const multer = require('multer')
+const { route } = require('../app')
+
+
+const router = Router()
+const storage = diskStorage({
+    destination: 'api/uploads',
+    filename: filename
+ })
+
+const upload = multer({fileFilter,storage:storage})
+
+router.post('/upload', upload.single('photo'), (request, response) => {
+    if(request.hasOwnProperty('fileValidationError')){
+        return response.status(400).json({error: request.fileValidationError})
+    }
+    else {
+        return response.status(201).json({success: true})
+    }
+})
+
+function filename(request, file, callback){
+    callback(null, file.originalname)
+}
+
+function fileFilter(request, file, callback){
+    if(file.mimetype !== 'image/png'){
+        request.fileValidationError = 'Wrong file type'
+        callback(null, false, new Error('Wrong file type'))
+    }
+    else {
+        callback(null, true)
+    }
+}
+
+module.exports = router
