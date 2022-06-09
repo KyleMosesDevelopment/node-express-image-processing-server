b7cf759881f53242bab380a6faeaacc94a24d0f3
Kyle Moses <kmoses@stedwards.edu>
2022-06-09 12:58:24 -0500

    smaller change

--- api/src/router.js
+++ api/src/router.js
@@ -1,6 +1,5 @@
 const { Router } = require('express')
 const multer = require('multer')
-const { route } = require('../app')
 
 
 const router = Router()
@@ -14,6 +13,16 @@ const storage = multer.diskStorage({
     filename
  })
 
+ const fileFilter = (request, file, callback) => {
+    if(file.mimetype !== 'image/png'){
+        request.fileValidationError = 'Wrong file type'
+        callback(null, false, new Error('Wrong file type'))
+    }
+    else {
+        callback(null, true)
+    }
+}
+
 const upload = multer({fileFilter,storage:storage})
 
 router.post('/upload', upload.single('photo'), (request, response) => {
@@ -24,15 +33,5 @@ router.post('/upload', upload.single('photo'), (request, response) => {
         return response.status(201).json({success: true})
     }
 })
-
-const fileFilter = (request, file, callback) => {
-    if(file.mimetype !== 'image/png'){
-        request.fileValidationError = 'Wrong file type'
-        callback(null, false, new Error('Wrong file type'))
-    }
-    else {
-        callback(null, true)
-    }
-}
 
 module.exports = router
