0745035873f36a154b373cda0925a977f235d299
Kyle Moses <kmoses@stedwards.edu>
2022-06-09 13:20:01 -0500

    mod 3

--- api/src/router.js
+++ api/src/router.js
@@ -1,5 +1,9 @@
 const { Router } = require('express')
+const { sendFile } = require('express/lib/response')
 const multer = require('multer')
+const path = require('path')
+
+const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html')
 
 
 const router = Router()
@@ -32,6 +36,10 @@ router.post('/upload', upload.single('photo'), (request, response) => {
     else {
         return response.status(201).json({success: true})
     }
+})
+
+router.get('/photo-viewer', (request, response) => {
+    response.sendFile(photoPath)
 })
 
 module.exports = router
