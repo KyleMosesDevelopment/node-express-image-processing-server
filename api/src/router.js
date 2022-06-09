dbac062a8e3fcf7726dee260348270eb1f5c7a37
Kyle Moses <kmoses@stedwards.edu>
2022-06-09 14:02:04 -0500

    mod 5

--- api/src/router.js
+++ api/src/router.js
@@ -2,6 +2,7 @@ const { Router } = require('express')
 const { sendFile } = require('express/lib/response')
 const multer = require('multer')
 const path = require('path')
+const imageProcessor = require('./imageProcessor')
 
 const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html')
 
@@ -29,12 +30,19 @@ const storage = multer.diskStorage({
 
 const upload = multer({fileFilter,storage:storage})
 
-router.post('/upload', upload.single('photo'), (request, response) => {
+router.post('/upload', upload.single('photo'), async (request, response) => {
     if(request.hasOwnProperty('fileValidationError')){
         return response.status(400).json({error: request.fileValidationError})
     }
     else {
         return response.status(201).json({success: true})
+    }
+
+    try {
+        await imageProcessor(request.file.filename)
+    }
+    catch {
+
     }
 })
 
